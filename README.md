17b9357949b70c76db5518b49f85eee3e824ea89
felipe <andradefelipe93@yahoo.com.br>
2018-03-24 14:13:02 -0300

    0.0.1

---
+++ README.md
@@ -0,0 +1,20 @@
+FreeCodeCamp API lesson - Timestamp
+==========================
+
+1- <b>User Story:</b> I can pass a string as a parameter, and it will check to see whether that string contains either a unix timestamp or a natural language date (example: January 1, 2016).
+
+2- <b>User Story:</b> If it does, it returns both the Unix timestamp and the natural language form of that date.
+
+3- <b>User Story:</b> If it does not contain a date or Unix timestamp, it returns null for those properties.
+
+[https://felipe-fcc-timestamp.glitch.me/](https://felipe-fcc-timestamp.glitch.me/)
+
+<h3>Example</h3>
+`https://felipe-fcc-timestamp.glitch.me/June%2006,%201993`
+
+`https://felipe-fcc-timestamp.glitch.me/739324800`
+
+<h3>Output</h3>
+`{"unix":739324800,"natural":"Sun Jun 06 1993"}`
+
+`{"unix":null,"natural":null}`
---
+++ server.js
@@ -0,0 +1,48 @@
+
+// init project
+const express = require('express');
+const app = express();
+
+
+app.use(express.static('public'));
+
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + '/index.html');
+});
+
+
+app.get("/:date", (req, res, next) => {
+
+  var natural_Date;
+  var unix;
+  if (/^\d{8,}$/.test(req.params.date)){
+    natural_Date = new Date(req.params.date * 1000).toUTCString();
+    unix = req.params.date;    
+  } else {
+    natural_Date = new Date(req.params.date);
+    unix = Math.floor(new Date(natural_Date)/1000);    
+  }
+  
+  if (natural_Date) {
+    
+    res.json({
+      unix: unix,
+      natural: new Date(natural_Date).toDateString()
+    });
+    
+  } else {
+    
+    res.json({
+      unix: null,
+      natural: null
+    })
+    
+  }
+  
+});
+
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, () => {
+  console.log(`Your app is listening on port ${listener.address().port}`)
+})
